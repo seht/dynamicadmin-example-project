97d90fcb7601f2431fe2e77803c5ec70883864c0
seht <seht@hyx.net>
2019-10-17 01:59:48 +0100

    tidy

--- mysite/example_bundle/tests.py
+++ mysite/example_bundle/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 # Create your tests here.
 
 from dynamicadmin.models import get_bundle_object, get_dynamic_models, get_dynamic_model, get_dynamic_model_objects
-from .models import ExampleBundle
 from dynamicadmin.models import BundleEntity, CharField
+from .models import ExampleBundle
 
 
 class ContentTest(TestCase):
@@ -12,13 +12,11 @@ class ContentTest(TestCase):
         from django.core.management import call_command
 
         bundle_model = ExampleBundle.objects.create(name='test_bundle_1', label="test bundle 1")
-
         charfield = CharField(bundle=bundle_model, name="test_charfield")
         charfield.save()
-
         bundle_model.fields.add(charfield)
-        bundle_model.create_dynamic_model(app_label='example_dynamic_models', base=BundleEntity)
 
+        bundle_model.create_dynamic_model(app_label='example_dynamic_models', base=BundleEntity)
         call_command('makemigrations', '--no-input')
         call_command('migrate')
 
@@ -26,7 +24,7 @@ class ContentTest(TestCase):
         dynamic_model.objects.create(label="example dynamic model", test_charfield="example content")
 
     def test_content(self):
-
         dynamic_model = get_bundle_object(ExampleBundle, 'test_bundle_1').get_dynamic_model('example_dynamic_models')
         new_content = dynamic_model.objects.get(test_charfield="example content")
+
         self.assertEqual(new_content.test_charfield, "example content")
